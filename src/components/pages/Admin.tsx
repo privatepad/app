67648e1b86ab56f558a4bc935dae3418a6ca9240
heycryptobob <heycryptobob@gmail.com>
2022-03-30 18:56:25 +0200

    admin factory address

--- src/components/pages/Admin.tsx
+++ src/components/pages/Admin.tsx
@@ -1,7 +1,15 @@
 function Admin() {
   return (
     <div>
-      <h1 className="py-8 text-2xl font-semibold text-slate-900">Admin</h1>
+      <div className="flex justify-between items-center">
+        <div>
+          <h1 className="py-8 text-2xl font-semibold text-slate-900">Admin</h1>
+        </div>
+        <div>
+          <div className="bg-slate-100 border rounded-lg text-slate-400 text-xs p-2">
+            <span className="mr-1 text-slate-900">Factory</span>{process.env.REACT_APP_FACTORY_ADDRESS}</div>
+        </div>
+      </div>
     </div>
   )
 }
